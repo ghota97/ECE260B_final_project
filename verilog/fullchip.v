30315aa7aa6fbf18a455ad6cdad9c2ece2f9667e
Gopabandhu Hota <ghota@ieng6-ece-11.ucsd.edu>
2022-03-06 11:30:51 -0800

    Updated with public files

---
+++ verilog/fullchip.v
@@ -0,0 +1,23 @@
+// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
+// Please do not spread this code without permission 
+module fullchip (clk, mem_in, inst, reset);
+
+parameter col = 8;
+parameter bw = 8;
+parameter bw_psum = 2*bw+4;
+parameter pr = 16;
+
+input  clk; 
+input  [pr*bw-1:0] mem_in; 
+input  [16:0] inst; 
+input  reset;
+
+
+core #(.bw(bw), .bw_psum(bw_psum), .col(col), .pr(pr)) core_instance (
+      .reset(reset), 
+      .clk(clk), 
+      .mem_in(mem_in), 
+      .inst(inst)
+);
+
+endmodule
